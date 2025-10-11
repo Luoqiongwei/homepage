67b0f422898437a10a58055931577a400aedc95c
Luoqiongwei <2300010761@stu.pku.edu.cn>
2025-10-11 16:49:01 +0800

    Add @astrojs/sitemap integration and update dependencies in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://luoqiongwei.github.io/homepage", 
   base: "/homepage/", 
   outDir: "./dist", 
+  integrations: [sitemap()],
   output: "static", 
   trailingSlash: 'always',
   build: {
